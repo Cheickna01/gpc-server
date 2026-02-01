1eb6dbf24eb51e22d2c2ba4ee5029069abcc9c68
Cheickna01 <diabyhamala0@gmail.com>
2026-02-01 01:02:06 +0100

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://gpc-production-2842.up.railway.app",
+      "https://gpc-ch.netlify.app/",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
